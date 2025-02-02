6814c93e0c5185ee5a5ab9d8548c12c72325bad0
Andrew H. Yang <tamfign@gmail.com>
2025-02-03 10:26:31 +1100

    A version that can apply

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "eks_vpc" {
 resource "aws_subnet" "eks_subnet_1" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "ap-southeast-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "eks-subnet-1"
@@ -25,7 +25,7 @@ resource "aws_subnet" "eks_subnet_1" {
 resource "aws_subnet" "eks_subnet_2" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "ap-southeast-2b"
   map_public_ip_on_launch = true
   tags = {
     Name = "eks-subnet-2"
@@ -103,28 +103,92 @@ resource "aws_iam_role_policy_attachment" "eks_node_role_policy" {
 }
 
 # Set Up Worker Nodes
-resource "aws_launch_configuration" "eks_launch_config" {
-  name          = "eks-launch-config"
-  image_id      = "ami-xxxxxxxx"  # You can specify an EKS-optimized AMI
-  instance_type = "t2.medium"
 
-  security_groups = [aws_security_group.eks_sg.id]
-  user_data = <<-EOF
-              #!/bin/bash
-              /etc/eks/bootstrap.sh my-eks-cluster
-              EOF
+# resource "aws_launch_configuration" "eks_launch_config" {
+#   name          = "eks-launch-config"
+#   image_id      = "ami-0a5395a56ccf8d85f"  # You can specify an EKS-optimized AMI
+#   instance_type = "t2.micro"
+#
+#   security_groups = [aws_security_group.eks_sg.id]
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               /etc/eks/bootstrap.sh my-eks-cluster
+#               EOF
+# }
+#
+# resource "aws_autoscaling_group" "eks_asg" {
+#   desired_capacity     = 2
+#   max_size             = 3
+#   min_size             = 2
+#   launch_configuration = aws_launch_configuration.eks_launch_config.id
+#   vpc_zone_identifier  = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
+#
+#   tag {
+#     key                 = "Name"
+#     value               = "eks-worker"
+#     propagate_at_launch = true
+#   }
+# }
+
+resource "aws_launch_template" "eks_nodes" {
+  name = "eks-node-template"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 20
+      volume_type = "gp3"
+      encrypted   = true
+    }
+  }
+
+  instance_type = "t2.micro"
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups            = [aws_security_group.eks_nodes.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "EKS-managed-node"
+    }
+  }
 }
 
-resource "aws_auto_scaling_group" "eks_asg" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 2
-  launch_configuration = aws_launch_configuration.eks_launch_config.id
-  vpc_zone_identifier  = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
+resource "aws_security_group" "eks_nodes" {
+  name        = "eks-node-group-sg"
+  description = "Security group for EKS node group"
+  vpc_id      = aws_vpc.eks_vpc.id
 
-  tag {
-    key                 = "Name"
-    value               = "eks-worker"
-    propagate_at_launch = true
+  ingress {
+    description = "Allow node to node communication"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-node-group-sg"
   }
 }
