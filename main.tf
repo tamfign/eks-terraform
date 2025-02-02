e0bf205ad4aebd082d4b5235046ebf3de1cddab6
Andrew Yang <tamfign@gmail.com>
2025-02-02 19:00:15 +1100

    Add create cluster and iam role in terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+# Define the VPC and Networking Resources
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -55,4 +56,75 @@ resource "aws_security_group_rule" "eks_sg_node_ingress" {
   to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = ["10.0.0.0/16"]
+}
+
+# Create an EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_subnet_1.id,
+      aws_subnet.eks_subnet_2.id,
+    ]
+    security_group_ids = [aws_security_group.eks_sg.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_role_policy]
+}
+
+# IAM Role and Policies for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_role_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+# Set Up Worker Nodes
+resource "aws_launch_configuration" "eks_launch_config" {
+  name          = "eks-launch-config"
+  image_id      = "ami-xxxxxxxx"  # You can specify an EKS-optimized AMI
+  instance_type = "t2.medium"
+
+  security_groups = [aws_security_group.eks_sg.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              /etc/eks/bootstrap.sh my-eks-cluster
+              EOF
+}
+
+resource "aws_auto_scaling_group" "eks_asg" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 2
+  launch_configuration = aws_launch_configuration.eks_launch_config.id
+  vpc_zone_identifier  = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id]
+
+  tag {
+    key                 = "Name"
+    value               = "eks-worker"
+    propagate_at_launch = true
+  }
 }
